456c066595ca8a1edefea3e3da5419863a39efa9
DennyJohnsonP <dennyjohnsondj4@gmail.com>
2020-10-17 20:27:49 +0530

    Added names for icon

--- transport.js
+++ transport.js
@@ -1,3 +1,11 @@
 function display() {
     alert("OOPS Server Under Development")
 }
+
+function login() {
+    alert("Did not sign in/loged in")
+}
+
+function server() {
+    alert("Server Under Development try again later")
+}
